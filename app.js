81498ef2b040d21caf98089a801fb586db71f9b2
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-05-31 05:25:42 +0500

    done

---
+++ app.js
@@ -0,0 +1,23 @@
+
+let mainParent = document.querySelector(".mainParent")
+let sideBar = document.querySelector("#sideBar")
+let main = document.querySelector(".main")
+
+function showForm(){
+    mainParent.style.display = "block"
+}
+function closeButton(){
+    mainParent.style.display = "none"
+}
+
+
+let isOpen = false
+function toggleButton(){
+    isOpen = !isOpen
+    if(isOpen){
+        sideBar.style.display = "block"
+    }
+    else{
+        sideBar.style.display = "none"
+    }
+}
